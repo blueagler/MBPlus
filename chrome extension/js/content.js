4cef9a586a66599a58e67c220838121b72a11b13
liujunhao <austinliu@blueagle.top>
2021-03-08 14:07:13 +0800

    update

--- chrome extension/js/content.js
+++ chrome extension/js/content.js
@@ -1,4 +1,4 @@
 let script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/gh/liujunhao-github/MBPlus@latest/MBPlus.js";
+script.src = "https://cdn.jsdelivr.net/gh/liujunhao-github/MBPlus@latest/dist/build.js";
 script.async = false;
 document.head.append(script);
