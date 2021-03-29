9b9d7fe1520eefeabbb6db51348914129b43acfa
liujunhao <austinliu@blueagle.top>
2021-03-29 00:37:53 +0000

    test

--- src/function/load.js
+++ src/function/load.js
@@ -1,29 +1,18 @@
-import dashboard from '&/dashboard'
-import task from '&/task'
-import global from '^/global'
-import calendar from '&/calendar'
-import { DropboxFileViewButton as hookfile } from '^/file'
+import dashboard from "&/dashboard";
+import task from "&/task";
+import global from "^/global";
+import calendar from "&/calendar";
+import { DropboxFileViewButton as hookfile } from "^/file";
 
 export default () => {
-  switch (document.querySelector("body").getAttribute("data-path")) {
-  case "student/dashboard/show":
-    dashboard();
-      break;
-  case "student/classes/classes/show":
-    dashboard();
-    break;
-  case "student/classes/core_tasks/show":
-    task();
-    break;
-  case "student/calendars/show":
-    calendar();
-      break;
-  case "student/classes/calendars/show":
-    calendar();
-    break;
-  case "student/classes/files/index":
-    hookfile();
-    break;
-  }
-  global()
-}
+  const load = {
+    "student/dashboard/show": dashboard,
+    "student/classes/classes/show": dashboard,
+    "student/classes/core_tasks/show": task,
+    "student/calendars/show": calendar,
+    "student/classes/calendars/show": calendar,
+    "student/classes/files/index": hookfile,
+  };
+  load[document.querySelector("body").getAttribute("data-path")]();
+  global();
+};
